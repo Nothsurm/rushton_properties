64c382dffa09622c6a203796f9882ebf67b2bc5f
Mike Rushton <mrush94ton@gmail.com>
2023-12-18 11:21:56 +0000

    Created sign-in page ui and functionaility

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -18,8 +18,8 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
-        const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validUser = await User.findOne({ email });   //tests if email exists
+        if (!validUser) return next(errorHandler(404, 'User not found!'));  //if email doesnt exist
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
@@ -27,7 +27,7 @@ export const signin = async (req, res, next) => {
         res
             .cookie('access_token', token, {httpOnly: true})
             .status(200)
-            .json(rest)
+            .json(rest)       //seperates password from the rest of the encyrption
     } catch (error) {
         next(error)
     }
